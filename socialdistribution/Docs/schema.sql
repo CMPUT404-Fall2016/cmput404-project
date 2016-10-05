d44e2dff0570cdc24faf948bcc066848ec19b532
Touqir Sajed <shuhash6@gmail.com>
2016-10-05 05:28:39 -0600

    modified 'Servers' table within schema.sql file

--- socialdistribution/Docs/schema.sql
+++ socialdistribution/Docs/schema.sql
@@ -129,8 +129,9 @@ CREATE TABLE Friend_Requests
 
 CREATE TABLE Servers
 (
-    server_id int,
+    server_id bigint,
     IP varchar(128), //Designed for 128bit IPv6 addressing
     server_index int,
+    PRIMARY KEY(server_id)
 );
 
