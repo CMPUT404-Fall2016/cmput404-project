48e00b26bf2cfdf2dc749c3cb4e26962c7526bb7
Brandon Williams <bwei.williams@gmail.com>
2016-10-21 12:08:27 -0600

    list group for posts

--- socialdistribution/app.py
+++ socialdistribution/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for, send_from_directory
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def root():
+  return app.send_static_file('index.html')
+
+@app.route('/<path:path>')
+def static_proxy(path):
+  # send_static_file will guess the correct MIME type
+  return app.send_static_file(path)
 
 if __name__ == "__main__":
     app.run()
-
