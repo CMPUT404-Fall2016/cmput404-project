b44c14e77015a049b00793d669ca4d59225b24e4
Brandon Williams <bwei.williams@gmail.com>
2016-10-21 19:36:17 -0600

    revert change to app.py

--- socialdistribution/app.py
+++ socialdistribution/app.py
@@ -1,14 +1,9 @@
-from flask import Flask, request, redirect, url_for, send_from_directory
+from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def root():
-  return app.send_static_file('index.html')
-
-@app.route('/<path:path>')
-def static_proxy(path):
-  # send_static_file will guess the correct MIME type
-  return app.send_static_file(path)
+def hello():
+    return "Hello World!"
 
 if __name__ == "__main__":
     app.run()
