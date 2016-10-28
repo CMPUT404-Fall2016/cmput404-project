648ab00a40a9b14268856fee01f75a2f3c1f96f3
Brandon Williams <bwei.williams@gmail.com>
2016-10-27 18:02:53 -0600

    checks for github username in cookie, queries database if missing

--- socialdistribution/scripts/github.js
+++ socialdistribution/scripts/github.js
@@ -1,40 +1,77 @@
 $(document).ready(function() {
 
-  var github_id = "stat3kk";
-  var url = "https://api.github.com/users/" + github_id + "/events";
+  // this is the author's github_username, empty string if there isn't one
+  var github_name = "";
+
+  //debug
+  // document.cookie = "github_name=stat3kk; expires=Thu, 18 Dec 2018 12:00:00 UTC";
+
+  (function getGithubUsername() {
+    // look for the github_name in cookies
+    var cookies = document.cookie.split(";");
+    for(var i=0; i < cookies.length; i++) {
+      var gname = cookies[i].split("=");
+      if(gname[0] == "github_name") {
+        github_name = gname[1];
+        return;
+      }
+    }
+    // // if not found, query the DB for it
+    // sendAJAX("GET", "http://service/author/github_username", "", function(text) {
+    //   username = JSON.parse(text);
+    //   console.log(username);
+    //   if(username) {
+    //     return username;
+    //   } else {
+    //     return "";
+    //   }
+    // });
+  })();
+
+  var github_url = "https://api.github.com/users/" + github_name + "/events";
 
   var postContainer = document.getElementById("posts");
   var githubContainer = document.getElementById("github-container");
 
-  var processEvents = function (json) {
-    var result = JSON.parse(json);
-    // var 30days = new Date().setDate(today.getDate()-30)
-    for(var i=0; i < result.length; ++i) {
-      var repo = "https://github.com/" + result[i].repo.name;
-      githubContainer.content.querySelector(".github-type").innerHTML = result[i].type;
-      githubContainer.content.querySelector(".github-dp").href = result[i].actor.url;
-      githubContainer.content.querySelector(".github-repo-url").href = repo;
-      githubContainer.content.querySelector(".github-repo-url").innerHTML = repo;
-      githubContainer.content.querySelector(".github-date").innerHTML = result[i].created_at;
-      var clone = document.importNode(githubContainer.content, true);
-      postContainer.appendChild(clone);
-    }
-  }
-
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.onreadystatechange = function(){
-    if (xhr.readyState==4) {
-      try {
-        if (xhr.status==200) {
-          processEvents(xhr.responseText);
+  // standard AJAX request
+  function sendAJAX(method, url, message, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.onreadystatechange = function(){
+      if (xhr.readyState==4) {
+        try {
+          if (xhr.status==200) {
+            if(callback) {
+              callback(xhr.responseText);
+            }
+          }
+        } 
+        catch(e) {
+          alert('Error: ' + e.name);
         }
-      } 
-      catch(e) {
-        alert('Error: ' + e.name);
       }
     }
+    xhr.send(JSON.stringify(message));
   }
 
-  xhr.send(null);
+  // get the events and process them to be displayed in github-containers
+  if(github_name) {
+    sendAJAX("GET", github_url, "", function(events) {
+      var result = JSON.parse(events);
+      for(var i=0; i < result.length; ++i) {
+        var repo_url = "https://github.com/" + result[i].repo.name;
+
+        // fill the container with details
+        githubContainer.content.querySelector(".github-type").innerHTML = result[i].type;
+        githubContainer.content.querySelector(".github-dp").href = result[i].actor.url;
+        githubContainer.content.querySelector(".github-repo-url").href = repo_url;
+        githubContainer.content.querySelector(".github-repo-url").innerHTML = repo_url;
+        githubContainer.content.querySelector(".github-date").innerHTML = result[i].created_at;
+        
+        // clone the template to render and append to the dom
+        var clone = document.importNode(githubContainer.content, true);
+        postContainer.appendChild(clone);
+      }
+    });
+  }
 });
