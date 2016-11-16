2a66fd331ba9245ab834326105055b97c92579bf
Weling5 <weiling588885@gmail.com>
2016-11-16 09:56:08 -0700

    friendpage.js done?

--- socialdistribution/static/js/friendsdata.js
+++ socialdistribution/static/js/friendsdata.js
@@ -1,45 +1,73 @@
+function sendAJAX(method, url, message, session_id, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.onreadystatechange = function(){
+    if (xhr.readyState==4) {
+      try {
+        if (xhr.status==200) {
+          if(callback) {
+            callback(JSON.parse(xhr.responseText));
+          }
+        }
+      }
+      catch(e) {
+        alert('Error: ' + e.name);
+      }
+    }
+  }
+  if(message) {
+    xhr.setHeader("Content-Type", "application/json");
+  }
+  xhr.send(JSON.stringify(message));
+}
+
 $(document).ready(function() {
                   
+//                  function getCookieid() {
+//                    // look for the github_name in cookies
+//                    var cookies = document.cookie.split(";");
+//                    for(var i=0; i < cookies.length; i++) {
+//                      var gname = cookies[i].split("=");
+//                      if(gname[0] == "github_name") {
+//                        return = gname[1];
+//                      }
+//                    }
+//                    return "";
+//                  }
+//                  
                   
-                  function sendAJAX(method, url, message, session_id, callback) {
-                    var xhr = new XMLHttpRequest();
-                    xhr.open(method, url);
-                    xhr.onreadystatechange = function(){
-                      if (xhr.readyState==4) {
-                        try {
-                          if (xhr.status==200) {
-                            if(callback) {
-                              callback(JSON.parse(xhr.responseText));
-                            }
-                        }
-                      }
-                        catch(e) {
-                          alert('Error: ' + e.name);
-                        }
-                      }
-                    }
-                    if(message) {
-                      xhr.setHeader("Content-Type", "application/json");
-                    }
-                    xhr.send(JSON.stringify(message));
-                  }
+                  
                   
 
-  var myTemplate = document.getElementById('friends-container');
+  var friendsTemplate = document.getElementById('friends-container');
                   
-  //var phname = myTemplate.content.querySelector(".profilehname");
+  //var phname = friendsTemplate.content.querySelector(".profilehname");
   //phname.textContent = "This is the changed pname";
+                  
                 
                   
-  var td = myTemplate.content.querySelector("#friendname");
-  friendsnametext = "Change Name";
-  td.textContent = friendsnametext;
+  //var td = friendsTemplate.content.querySelector("#friendname");
+  //friendsnametext = "Change Name";
+  //td.textContent = friendsnametext;
             
-  var normalContent = document.getElementById('friendstab');
+  //var normalContent = document.getElementById('friendstab');
                   
-  var clonedTemplate = myTemplate.content.cloneNode(true);
-  normalContent.appendChild(clonedTemplate)
+  //var clonedTemplate = friendsTemplate.content.cloneNode(true);
+  //normalContent.appendChild(clonedTemplate)
                   
+                  var friendsTemplate = document.getElementById('friends-container');
+                  
+                  sendAJAX("GET", "/getFriends", "", function(events) {
+                    for(var i=0; i < events.length; ++i) {
+                           var friendlink = "http://127.0.0.1:5000/author/" + result[i].authorid;
+                           friendsTemplate.content.querySelector("#friendname").textContent = result[i].authorname;
+                           friendsTemplate.content.querySelector("#friendnamelink").href = friendlink;
+                           
+                           var clonedTemplate = friendsTemplate.content.cloneNode(true);
+                           normalContent.appendChild(clonedTemplate)
+                    }
+                           
+                  });
                   /*var text1 = "Changed1";
                   var text2 = "Changed2";
                   var text3 = "Changed3";
@@ -50,6 +78,72 @@ $(document).ready(function() {
                   document.getElementsByName('githubid')[0].placeholder=text3;
                   document.getElementsByName('bio')[0].placeholder=text4;*/
 });
+
+$("#fingtab").click(function(e) {
+//                    var friendsTemplate = document.getElementById('friends-container');
+//                    
+//                    //var phname = friendsTemplate.content.querySelector(".profilehname");
+//                    //phname.textContent = "This is the changed pname";
+//                    
+//                    
+//                    
+//                    var td = friendsTemplate.content.querySelector("#friendname");
+//                    friendsnametext = "Change Name";
+//                    td.textContent = friendsnametext;
+//                    
+//                    var normalContent = document.getElementById('following');
+//                    
+//                    var clonedTemplate = friendsTemplate.content.cloneNode(true);
+//                    normalContent.appendChild(clonedTemplate)
+                    e.preventDefault();
+                    var friendsTemplate = document.getElementById('following-container');
+                    
+                    sendAJAX("GET", "/getFollowing", "", function(events) {
+                             for(var i=0; i < events.length; ++i) {
+                             var friendlink = "http://127.0.0.1:5000/author/" + result[i].authorid;
+                             friendsTemplate.content.querySelector("#friendname").textContent = result[i].authorname;
+                             friendsTemplate.content.querySelector("#friendnamelink").href = friendlink;
+                             
+                             var clonedTemplate = friendsTemplate.content.cloneNode(true);
+                             normalContent.appendChild(clonedTemplate)
+                             }
+                             
+                             });
+                    });
+
+$("#fertab").click(function(e) {
+                    e.preventDefault();
+                    var friendsTemplate = document.getElementById('follower-container');
+                    
+                    sendAJAX("GET", "/getFollower", "", function(events) {
+                             for(var i=0; i < events.length; ++i) {
+                             var friendlink = "http://127.0.0.1:5000/author/" + result[i].authorid;
+                             friendsTemplate.content.querySelector("#friendname").textContent = result[i].authorname;
+                             friendsTemplate.content.querySelector("#friendnamelink").href = friendlink;
+                             
+                             var clonedTemplate = friendsTemplate.content.cloneNode(true);
+                             normalContent.appendChild(clonedTemplate)
+                             }
+                             
+                             });
+                    });
+
+$("#fdtab").click(function(e) {
+                  e.preventDefault();
+                   var friendsTemplate = document.getElementById('friends-container');
+                   
+                   sendAJAX("GET", "/getFriends", "", function(events) {
+                            for(var i=0; i < events.length; ++i) {
+                            var friendlink = "http://127.0.0.1:5000/author/" + result[i].authorid;
+                            friendsTemplate.content.querySelector("#friendname").textContent = result[i].authorname;
+                            friendsTemplate.content.querySelector("#friendnamelink").href = friendlink;
+                            
+                            var clonedTemplate = friendsTemplate.content.cloneNode(true);
+                            normalContent.appendChild(clonedTemplate)
+                            }
+                            
+                            });
+                   });
 
 
 //
