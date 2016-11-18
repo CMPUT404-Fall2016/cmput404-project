f18724f2606b5930d462e6cc825b18c1403e2190
BoyuanADT <bgu1@ualberta.ca>
2016-11-18 14:17:02 -0700

    All public posts for bandon to test

--- socialdistribution/Server/cts/c2sM.py
+++ socialdistribution/Server/cts/c2sM.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from flask_restful import Resource, Api, abort, reqparse
 from model import *
 from pch import *
+import random, os
 
 app = Flask(__name__)
 api = Api(app)
@@ -28,16 +29,17 @@ class Post(Resource):
 
 
 
-class Post_List(Resource):
+class All_Post(Resource):
 	def get(self):
 		rtl = [] 
-		data = handler.getVisiblePosts(request.form)
+		data = handler.getAllPosts()
 		for entry in data:
 			rtl.append({
 									"post_id" :	entry[0].post_id,
 									"title" :	entry[0].title,
 									"text"	:	entry[0].text,
-									"creation_time" : entry[0].creation_time
+									"creation_time" : entry[0].creation_time,
+									"author_id"	: entry[0].author_id
 								})	
 
 		return jsonify(rtl)	
@@ -47,7 +49,8 @@ class Post_List(Resource):
 	def post(self):	
 		data = request.form
 		return handler.make_post(data), 201	
-		
+
+
 
 class Comment(Resource):
 	def post(self):
@@ -57,25 +60,23 @@ class Comment(Resource):
 
 api.add_resource(Post, '/<string:post_id>')
 api.add_resource(Comment, '/api/comment')
-api.add_resource(Post_List, '/')
+api.add_resource(All_Post, '/service/posts')
 
 if __name__ == '__main__':
-	for i in range(1, 2):
+	for i in range(1, 55):
 		currentTime = datetime.now()
-		post1 = {}
-		post1["post_id"] = i
-		post1["author_id"] = 1
-		post1["title"] = "test1"
-		post1["text"]="TEXT1"
-		post1["creation_time"]=currentTime 
-		post1["view_permission"]=1
-		post1["post_type"]=1
-		post1["numberOf_comments"]=1
-		post1["numberOf_URL"]=2
-		post1["numberOf_images"]=2
-		apost = Posts(post1)
-		db.session.add(apost)
-		db.session.commit()
+		post = {}
+		post["author_id"] = i
+		post["title"] = "test" + str(i)
+		post["text"]="TEXT" + str(i)
+		post["view_permission"]=random.randint(1, 5)
+		post["post_type"]=1
+		post["numberOf_comments"]=0
+		post["numberOf_URL"]=0
+		post["numberOf_images"]=0
+		post["images"] = []
+		post["images"].append(os.urandom(100000))
+		handler.make_post(post)
 
 	app.run(debug=True)
 
