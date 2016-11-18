142e5b5a9427283cb8ee2cdf2980f90c1c38f380
Brandon Williams <bwei.williams@gmail.com>
2016-11-17 19:13:02 -0700

    new post format

--- socialdistribution/static/js/post.js
+++ socialdistribution/static/js/post.js
@@ -1,3 +1,8 @@
+// functionality of index.html
+
+var postList = document.getElementById("posts"),
+    postTemplate = document.getElementById("post-container");
+
 $("#post-submit").click(function(e) {
   e.preventDefault();
 
@@ -19,9 +24,26 @@ $("#post-submit").click(function(e) {
   window.location.reload();
 });
 
+// get the posts from authors I follow
+$(document).ready(function() {
+  // page=<Page_No>&size=<Page_Zize>
+  sendAJAX("GET", "/allPosts", "", function(posts) {
+    for(var i=0; i < posts.length; ++i) {
+         // fill the container with details
+      postTemplate.content.querySelector(".post-title").textContent = posts[i].title;
+      postTemplate.content.querySelector(".post-author").textContent = posts[i].author.displayname;
+      postTemplate.content.querySelector(".post-author-url").href = posts[i].author.url;
+      postTemplate.content.querySelector(".post-content").textContent = posts[i].content;
+
+      // clone the template to render and append to the dom
+      var clone = document.importNode(postTemplate.content, true);
+      postList.appendChild(clone);
+    }
+  });
+});
+
 // http://stackoverflow.com/questions/34972072/how-to-send-image-to-server-with-http-post-in-javascript-and-store-base64-in-mon
 // 11/01/2016
-
 // converts an image to Base64 encoding for sending in http request
 // not used
 function convertToBase64(url, imagetype, callback) {
