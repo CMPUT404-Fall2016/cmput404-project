15c7f32b3604f79cd79a402a3340f965606af1dd
Brandon Williams <bwei.williams@gmail.com>
2016-11-18 00:10:47 -0700

    binding functions to links

--- socialdistribution/static/js/index.js
+++ socialdistribution/static/js/index.js
@@ -15,18 +15,28 @@ $("#post-submit").click(function(e) {
   var timestamp = new Date();
   postData["published"] = timestamp.toISOString();
 
-  // debug
   // console.log(postData);
   sendAJAX("POST", "/makePost", postData, null)
   window.location.reload();
 });
 
-$(".comments-btn").click(function(e) {
+// bind the onclick to set post host and id in localStorage
+// and link the user to the post's page
+$(".comments").click(function(e) {
   e.preventDefault();
   // set this for later
-  localStorage("fetch-post-host", this.data("post-host"));
-  localStorage("fetch-post-id", this.data("post-id"));
+  localStorage.setItem("fetch-post-host", $(this).data("post-host"));
+  localStorage.setItem("fetch-post-id", $(this).data("post-id"));
   window.location.href("post.html")
+});
+
+// bind the onclick to set author id in localStorage
+// and link the user to the author's profile
+$(".post-author-url").click(function(e) {
+  e.preventDefault();
+  // set this for later
+  localStorage.setItem("fetch-author-id", $(this).data("post-author-id"));
+  window.location.href("authorpage.html")
 });
 
 // searches cookies for a github_username
@@ -51,12 +61,17 @@ $(document).ready(function() {
     for(var i=0; i < posts.length; ++i) {
       // fill the container with details
       postTemplate.content.querySelector(".post-title").textContent = posts[i].title;
+      postTemplate.content.querySelector(".post-description").textContent = posts[i].description;
       postTemplate.content.querySelector(".post-author").textContent = posts[i].author.displayname;
       postTemplate.content.querySelector(".post-content").textContent = posts[i].content;
 
-      // attach data to the button so it can be referenced when clicked
-      postTemplate.content.querySelector(".comments").data("post-host", posts[i].author.host);
-      postTemplate.content.querySelector(".comments").data("post-id", posts[i].id);
+      // attach data to the links so it can be referenced when clicked
+      var authorBtn = postTemplate.content.querySelector(".post-author-url");
+      $(authorBtn).data("post-author-id", posts[i].author.id);
+
+      var commentsBtn = postTemplate.content.querySelector(".comments");
+      $(commentsBtn).data("post-host", posts[i].author.host);
+      $(commentsBtn).data("post-id", posts[i].id);
 
       var clone = document.importNode(postTemplate.content, true);
       postList.appendChild(clone);
