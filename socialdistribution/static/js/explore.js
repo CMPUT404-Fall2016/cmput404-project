ce6fcff62af677ee67200be61ec2c932a702de8d
Brandon Williams <bwei.williams@gmail.com>
2016-11-17 19:25:49 -0700

    login-check adjustments

--- socialdistribution/static/js/explore.js
+++ socialdistribution/static/js/explore.js
@@ -5,10 +5,9 @@ var postList = document.getElementById("posts"),
 
 // get all the public posts on the server
 $(document).ready(function() {
-  // page=<Page_No>&size=<Page_Zize>
-  sendAJAX("GET", "/allPosts", "", function(posts) {
+  sendAJAX("GET", "/explore", "", function(posts) {
     for(var i=0; i < posts.length; ++i) {
-         // fill the container with details
+      // fill the container with details
       postTemplate.content.querySelector(".post-title").textContent = posts[i].title;
       postTemplate.content.querySelector(".post-author").textContent = posts[i].author.displayname;
       postTemplate.content.querySelector(".post-author-url").href = posts[i].author.url;
--- socialdistribution/static/js/login-check.js
+++ socialdistribution/static/js/login-check.js
@@ -1,11 +1,18 @@
+/* INCLUDE THIS ON ALL PAGES THAT REQUIRE AUTHENTICATION
+  index.html
+  profilepage.html
+  etc
+  not author.html
+*/
+
 (function() {
   // check cookies for an author_id, if not found then redirect user
   var cookies = document.cookie.split(";");
   for(var i=0; i < cookies.length; i++) {
     var cname = cookies[i].split("=");
-    if(cname[0] == "cookie_cmput404") {
+    if(cname[0] == "cookie_cmput404_session_id") {
       return;
     }
   }
-  window.location.replace("login.html");
+  window.location.href = "login.html";
 })();
