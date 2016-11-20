eb445459433b6d938353f2167728f400fed45e02
Brandon Williams <bwei.williams@gmail.com>
2016-11-20 15:41:47 -0700

    ..actually committing explore author and comment links

--- socialdistribution/static/js/explore.js
+++ socialdistribution/static/js/explore.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
       // fill the container with details
       postTemplate.content.querySelector(".post-title").textContent = posts[i].title;
       // postTemplate.content.querySelector(".post-description").textContent = posts[i].description;
-      postTemplate.content.querySelector(".post-author").textContent = posts[i].author_id;
+      postTemplate.content.querySelector(".post-author").text = posts[i].author_id;
       postTemplate.content.querySelector(".post-content").textContent = posts[i].text;
 
       // attach data to the links so it can be referenced when clicked
@@ -25,24 +25,25 @@ $(document).ready(function() {
       var clone = document.importNode(postTemplate.content, true);
       postList.appendChild(clone);
     }
+
+	  // bind the onclick to set author id in localStorage
+    // and link the user to the author's profile
+    $(".post-author").click(function(e) {
+    	e.preventDefault();
+    	console.log("clicked author");
+    	// set this for authorpage to use
+    	localStorage.setItem("fetch-author-id", $(this).data("post-author-id"));
+    	window.location.href = "authorpage.html";
+    });
+
+    // bind the onclick to set post host and id in localStorage
+    // and link the user to the post's page
+    $(".comments").click(function(e) {
+      e.preventDefault();
+      // set this for later
+      // localStorage.getItem("fetch-post-host", $(this).data("post-host"));
+      localStorage.getItem("fetch-post-id", $(this).data("post-id"));
+      window.location.href("post.html")
+    });
   });
-});
-
-// bind the onclick to set post host and id in localStorage
-// and link the user to the post's page
-$(".comments").click(function(e) {
-  e.preventDefault();
-  // set this for later
-  // localStorage.getItem("fetch-post-host", $(this).data("post-host"));
-  localStorage.getItem("fetch-post-id", $(this).data("post-id"));
-  window.location.href("post.html")
-});
-
-// bind the onclick to set author id in localStorage
-// and link the user to the author's profile
-$(".post-author-url").click(function(e) {
-  e.preventDefault();
-  // set this for later
-  localStorage.setItem("fetch-author-id", $(this).data("post-author-id"));
-  window.location.href("authorpage.html")
 });
