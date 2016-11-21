138cb5f667785f03fc4278b6eaefa70912cba151
Brandon Williams <bwei.williams@gmail.com>
2016-11-20 19:25:51 -0700

    base64 encoded images

--- socialdistribution/static/js/index.js
+++ socialdistribution/static/js/index.js
@@ -1,21 +1,29 @@
 // functionality of index.html
 
+var postForm = document.getElementById("post-form");
+
 $("#post-submit").click(function(e) {
   e.preventDefault();
 
   // encode form data as a JSON object
-  var postForm = document.getElementById("post-form"),
-      postData = {};
+  var postData = {};
   postData["author_id"] = localStorage.getItem("author_id");
   postData["title"] = postForm.elements["title"].value;
   postData["description"] = postForm.elements["desc"].value;
-  postData["contentType"] = postForm.elements["text-type"].value;
+  // postData["contentType"] = postForm.elements["text-type"].value;
   postData["content"] = postForm.elements["post-text"].value;
   postData["visibility"] = postForm.elements["visibility"].value;
 
-  // console.log(JSON.stringify(postData));
-  sendAJAX("POST", "/service/posts", postData, null);
-  window.location.reload();
+  // convert the image to base64 string and attach to the data
+  var reader = new FileReader();
+  reader.addEventListener("load", function () {
+    postData["image"] = reader.result;
+    console.log(JSON.stringify(postData));
+  }, false);
+  reader.readAsDataURL(postForm.elements["image"].files[0]);
+  // sendAJAX("POST", "/service/posts", postData, null);
+  // done with request, reload
+  // window.location.reload();
 });
 
 // searches cookies for a github_username
@@ -35,7 +43,6 @@ function getGithubUsername() {
 $(document).ready(function() {
   var postList = document.getElementById("posts");
   var postTemplate = document.getElementById("post-container");
-  // page=<Page_No>&size=<Page_Zize>
   sendAJAX("GET", "service/author/posts", "", function(posts) {
     for(var i=0; i < posts.length; ++i) {
       // fill the container with details
@@ -110,34 +117,3 @@ $(document).ready(function() {
     });
   }
 });
-
-
-// http://stackoverflow.com/questions/34972072/how-to-send-image-to-server-with-http-post-in-javascript-and-store-base64-in-mon
-// 11/01/2016
-// converts an image to Base64 encoding for sending in http request
-// not used
-function convertToBase64(url, imagetype, callback) {
-
-    var img = document.createElement('IMG'),
-        canvas = document.createElement('CANVAS'),
-        ctx = canvas.getContext('2d'),
-        data = "";
-
-    img.crossOrigin = 'Anonymous'
-
-    // Because image loading is asynchronous, we define an event listening function that will be called when the image has been loaded
-    img.onLoad = function() {
-        // When the image is loaded, this function is called with the image object as its context or 'this' value
-        canvas.height = this.height;
-        canvas.width = this.width;
-        ctx.drawImage(this, 0, 0);
-        data = canvas.toDataURL(imagetype);
-        callback(data);
-    };
-
-    // We set the source of the image tag to start loading its data. We define
-    // the event listener first, so that if the image has already been loaded
-    // on the page or is cached the event listener will still fire
-
-    img.src = url;
-}
