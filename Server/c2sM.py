de309e091bd367a6c9973d69377ba2f7c0c559a8
Weling5 <weiling588885@gmail.com>
2016-11-22 15:48:18 -0700

    push

--- Server/c2sM.py
+++ Server/c2sM.py
@@ -1,6 +1,6 @@
 import flask
 import json
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, requests
 from flask_restful import Resource, Api, abort, reqparse
 from model import *
 from pch import *
