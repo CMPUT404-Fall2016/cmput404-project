62f4037123dd0fea3f0420e9c22c3c936d9b395a
Touqir Sajed <shuhash6@gmail.com>
2016-11-22 13:38:46 -0700

    updated db file again

--- db.py
+++ db.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask.ext.heroku import Heroku
+import os
 
 # from yourapplication.database import db_session
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://' # In memory DB
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://' # In memory DB
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///DB/tables.db' # relative path
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.sqlite' # In memory DB
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+heroku = Heroku(app)
 db = SQLAlchemy(app)
 # db.create_all()
 
