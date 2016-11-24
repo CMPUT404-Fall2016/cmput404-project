9cf70c21da65a27f528b43913b46c4c55bacc039
Touqir Sajed <shuhash6@gmail.com>
2016-11-24 02:11:40 -0700

    some changes

--- Server/author_endpointHandlers.py
+++ Server/author_endpointHandlers.py
@@ -197,6 +197,7 @@ def processFriendRequest(param, APP_state):
     from_serverIP = param["from_serverIP"]
     from_server_index=db.session.query(Servers).filter(Servers.IP == from_serverIP).all()[0].server_index
 
+    print ">>>1"
     query_param = {}
     query_param['server_author_1'] = [from_server_index, param['from_author']]
     results = Author_Relationships.query(query_param) 
@@ -205,7 +206,8 @@ def processFriendRequest(param, APP_state):
             results[0].relationship_type = 3
             db.session.commit()
         return True
-
+    
+    print ">>>2"
     query_param = {}
     query_param['server_author_2'] = [from_server_index, param['from_author']]
     results = Author_Relationships.query(query_param) 
@@ -220,7 +222,7 @@ def processFriendRequest(param, APP_state):
             db.session.commit()
         return True
 
-
+    print ">>>3"
     if APP_state['local_server_Obj'].IP == from_serverIP:
         print "process 1"
         datum={}
@@ -239,6 +241,7 @@ def processFriendRequest(param, APP_state):
         db.session.add(new_relationship)
         db.session.commit()
 
+    print ">>>4"
     if to_serverIP == APP_state['local_server_Obj'].IP :
         datum={}
         datum = {
