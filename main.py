e937c83cc9932ef12863b560cddbc0af3c88873e
r-hu1 <randyhu3089@gmail.com>
2016-11-23 19:44:16 -0700

    admin auth fixing

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ def requires_auth(f):
     return decorated
 #this is for server to server basic auth
 #-----------------------------------------need @requires_auth
-
+'''
 # quick fix for build_in flask
 class ModelView(flask_admin.contrib.sqla.ModelView):
     def is_accessible(self):
@@ -102,30 +102,37 @@ class ModelView(flask_admin.contrib.sqla.ModelView):
             return authenticate()
     
         return True
+'''
 
-
-
+@requires_auth
 class UserView(ModelView):
     can_create = True
 
+@requires_auth
 class PostView(ModelView):
     can_create = True
 
+@requires_auth
 class ImageView(ModelView):
     can_create = True
 
+@requires_auth
 class URLView(ModelView):
     can_create = True
 
+@requires_auth
 class ServerView(ModelView):
     can_create = True
 
+@requires_auth
 class GlobalView(ModelView):
     can_create = True
 
+@requires_auth
 class FriendRelationshipsView(ModelView):
     can_create = True
 
+@requires_auth
 class FriendRequestsView(ModelView):
     can_create = True
 
@@ -209,6 +216,7 @@ def getCookie(Operation_str):
 
 
 @app.route("/login", methods=['POST'])
+
 def Login():
     """ 
     Responsible for loggin in user. Creates a session ID and sends back all the information as a cookie.
