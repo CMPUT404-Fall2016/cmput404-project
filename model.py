af669f2a7e5046ae9c1cf93fdaa28ce0154936aa
Touqir Sajed <shuhash6@gmail.com>
2016-11-23 19:39:37 -0700

    Fixed indentation

--- model.py
+++ model.py
@@ -31,30 +31,30 @@ APP_state['no_friend_Requests'] : Number of friend requests send to this server.
 
 def initAdmin():
     APP_state = loadGlobalVar()
-	try :
-		f = open("admin_credentials.txt")
-		text = f.read()
-		splitted = text.split(',')
-		login = splitted[0].split(':')[1].strip()
-		password = splitted[1].split(':')[1].strip()
-		APP_state['admin_credentials'] = [login, password]
-		f.close()
-		print APP_state["admin_credentials"] 
-		saveGlobalVar()
+    try :
+        f = open("admin_credentials.txt")
+        text = f.read()
+        splitted = text.split(',')
+        login = splitted[0].split(':')[1].strip()
+        password = splitted[1].split(':')[1].strip()
+        APP_state['admin_credentials'] = [login, password]
+        f.close()
+        print APP_state["admin_credentials"] 
+        saveGlobalVar()
 
-	except Exception as e:
-		print "error while loading admin : ", e
+    except Exception as e:
+        print "error while loading admin : ", e
 
 
 def DELETE_ALL():
-	db.session.query(Author_Relationships).delete()
-	db.session.query(Authors).delete()
-	db.session.query(Comments).delete()
-	db.session.query(Friend_Requests).delete()
-	db.session.query(Images).delete()
-	db.session.query(Posts).delete()
-	db.session.query(Servers).delete()
-	db.session.query(URL).delete()
+    db.session.query(Author_Relationships).delete()
+    db.session.query(Authors).delete()
+    db.session.query(Comments).delete()
+    db.session.query(Friend_Requests).delete()
+    db.session.query(Images).delete()
+    db.session.query(Posts).delete()
+    db.session.query(Servers).delete()
+    db.session.query(URL).delete()
 
 
 def initAppState():
@@ -65,42 +65,42 @@ def initAppState():
     saveGlobalVar(APP_state)
 
 def initServerObj():
-	global APP_state
-	server={}
-	server["server_id"] = uuid.uuid4().hex
-	server["IP"] = "http://127.0.0.1:5000"  
-	APP_state["no_servers"] += 1
-	server["server_index"] = 0
-	myServer=Servers(server)
-	APP_state['local_server_Obj'] = myServer
-	db.session.add(myServer)
-	db.session.commit()
+    global APP_state
+    server={}
+    server["server_id"] = uuid.uuid4().hex
+    server["IP"] = "http://127.0.0.1:5000"  
+    APP_state["no_servers"] += 1
+    server["server_index"] = 0
+    myServer=Servers(server)
+    APP_state['local_server_Obj'] = myServer
+    db.session.add(myServer)
+    db.session.commit()
 
 def createDefaultAuthor():
-	defaultAuthor1=Authors(author1)
-	defaultAuthor2=Authors(author2)
-	db.session.add(defaultAuthor1)
-	db.session.add(defaultAuthor2)
-	db.session.commit()
+    defaultAuthor1=Authors(author1)
+    defaultAuthor2=Authors(author2)
+    db.session.add(defaultAuthor1)
+    db.session.add(defaultAuthor2)
+    db.session.commit()
 
 def loadGlobalVar():
-	global_var = db.session.query(Global_var).filter(Global_var.id == 0).all()[0]
-	var_dict = json.loads(global_var.content)
-	return var_dict
+    global_var = db.session.query(Global_var).filter(Global_var.id == 0).all()[0]
+    var_dict = json.loads(global_var.content)
+    return var_dict
 
 def saveGlobalVar(var_dict):
-	global_var = db.session.query(Global_var).filter(Global_var.id == 0).all()[0]
-	toStore = json.dumps(var_dict)
-	global_var.content = toStore
-	db.session.commit()
+    global_var = db.session.query(Global_var).filter(Global_var.id == 0).all()[0]
+    toStore = json.dumps(var_dict)
+    global_var.content = toStore
+    db.session.commit()
 
 def initDB():
-	db.create_all()
-	new_global = Global_var()
-	new_global.content = "{}"
-	new_global.id = 0
-	db.session.add(new_global)
-	db.session.commit()
+    db.create_all()
+    new_global = Global_var()
+    new_global.content = "{}"
+    new_global.id = 0
+    db.session.add(new_global)
+    db.session.commit()
 
 
 
