2d4d79c9a35f4b093fbecdd7fc6ce9435b3d4905
r-hu1 <randyhu3089@gmail.com>
2016-11-23 19:16:36 -0700

    admin auth more fix

--- main.py
+++ main.py
@@ -94,6 +94,7 @@ def requires_auth(f):
 #this is for server to server basic auth
 #-----------------------------------------need @requires_auth
 
+'''
 # quick fix for build_in flask
 class ModelView(flask_admin.contrib.sqla.ModelView):
     def is_accessible(self):
@@ -103,32 +104,39 @@ class ModelView(flask_admin.contrib.sqla.ModelView):
             return authenticate()
     
         return True
-
+'''
 
 
 class UserView(ModelView):
     can_create = True
+    @requires_auth
 
 class PostView(ModelView):
     can_create = True
+    @requires_auth
 
 class ImageView(ModelView):
     can_create = True
-
+    @requires_auth
 class URLView(ModelView):
     can_create = True
+    @requires_auth
 
 class ServerView(ModelView):
     can_create = True
+    @requires_auth
 
 class GlobalView(ModelView):
     can_create = True
+    @requires_auth
 
 class FriendRelationshipsView(ModelView):
     can_create = True
+    @requires_auth
 
 class FriendRequestsView(ModelView):
     can_create = True
+    @requires_auth
 
 class Back(BaseView):
     @expose('/')
