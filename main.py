ec0bc495302fdbfbf9a5ab4a910865a0ad69453a
r-hu1 <randyhu3089@gmail.com>
2016-11-24 11:33:24 -0700

    merge

--- main.py
+++ main.py
@@ -75,6 +75,7 @@ def check_auth(username, password, forign_server):
 
     print "This is an example wsgi app served from {} to {}".format(socket.gethostname(), request.url_root)
     print "foreign server : "
+    forign_server = forign_server[:-1]
     print forign_server
     db_server = db.session.query(Servers).filter(Servers.IP == forign_server).all()
     
