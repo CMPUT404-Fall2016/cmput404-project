ded05e29bc43b377bfa955c8bd165d5d3699193f
Weling5 <weiling588885@gmail.com>
2016-11-24 23:31:29 -0700

    test

--- static/js/explore.js
+++ static/js/explore.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
            var commonmarkresult = writer.render(parsed);
            
       //postTemplate.content.querySelector(".post-content").textContent = commonmarkresult;
-      postTemplate.content.querySelector(".post-content").textContent = results.posts[i].content;
+      postTemplate.content.querySelector(".post-content").innerHTML = results.posts[i].content;
 
       // attach data to the links so it can be referenced when clicked
       var authorBtn = postTemplate.content.querySelector(".post-author");
