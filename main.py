3bea611ea454950cafa2fd6a9cbeef9b24563177
Touqir Sajed <shuhash6@gmail.com>
2016-11-26 18:23:27 -0700

    some changes for sending friend request to foreign author

--- main.py
+++ main.py
@@ -640,25 +640,25 @@ def FollowUser():
 
     print "from FollowUser!"
     printSessionIDs(APP_state)
-    try :
-        param={}
-        param["from_author"] = data["author"]["id"]
-        param["from_author_name"] = data["author"]["displayName"]
-        param["from_serverIP"] = data["author"]["host"]
-        param["to_author"] = data["friend"]["id"]
-        param["to_author_name"] = data["friend"]["displayName"]
-        param["to_serverIP"] = data["friend"]["host"]
-        
-        # print data
-        result = processFriendRequest(param, APP_state)
+    # try :
+    param={}
+    param["from_author"] = data["author"]["id"]
+    param["from_author_name"] = data["author"]["displayName"]
+    param["from_serverIP"] = data["author"]["host"]
+    param["to_author"] = data["friend"]["id"]
+    param["to_author_name"] = data["friend"]["displayName"]
+    param["to_serverIP"] = data["friend"]["host"]
+    
+    # print data
+    result = processFriendRequest(param, APP_state)
 
-        return getResponse(status_code=200)
+    return getResponse(status_code=200)
 
-    except Exception as e:
+    # except Exception as e:
 
-        print "Exception from followUser"
-        print data
-        return getResponse(status_code=400)
+    # print "Exception from followUser"
+    # print data
+    # return getResponse(status_code=400)
 
     # if result == True:
     #     return getResponse(body={"status": "SUCCESS"}, status_code=200)
