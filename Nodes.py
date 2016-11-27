0e346c871868202f2e292ad4b6526fd1a08d5a7e
Touqir Sajed <shuhash6@gmail.com>
2016-11-26 18:04:42 -0700

    some updates

--- Nodes.py
+++ Nodes.py
@@ -120,15 +120,15 @@ secure_springs = {
 bloggyblog404 = {
 			'GET/friends/A' : ["http://api-bloggyblog404.herokuapp.com/friends/", "/?format=json"],
 			'GET/friends/A1/A2' : ["http://api-bloggyblog404.herokuapp.com/friends/", "/?format=json"],
-			'POST/friends/A' : ["http://api-bloggyblog404.herokuapp.com/friends/" , "/?format=json"],
-			'POST/friendrequest' : ["http://api-bloggyblog404.herokuapp.com/friendrequest", "/?format=json"],
+			'POST/friends/A' : ["http://api-bloggyblog404.herokuapp.com/friends/" , "/"],
+			'POST/friendrequest' : ["http://api-bloggyblog404.herokuapp.com/friendrequest", "/"],
 			'GET/author/A' : ["http://api-bloggyblog404.herokuapp.com/author/", "/?format=json"],
 			'GET/posts' : ["http://api-bloggyblog404.herokuapp.com/posts", "/?format=json"],
-			'GET/author/posts' : ["http://api-bloggyblog404.herokuapp.com/author/posts", "/?format=json"],
-			'GET/author/A/posts' : ["http://api-bloggyblog404.herokuapp.com/author/", "/posts/?format=json"],
+			'GET/author/posts' : ["http://api-bloggyblog404.herokuapp.com/author/posts", "/?format=json"], #Not implemented
+			'GET/author/A/posts' : ["http://api-bloggyblog404.herokuapp.com/author/", "/posts/?format=json"], #Not implemented
 			'GET/posts/P' : ["http://api-bloggyblog404.herokuapp.com/posts/", "/?format=json"],
 			'GET/posts/P/comments' : ["http://api-bloggyblog404.herokuapp.com/posts/", "/comments/?format=json"],
-			'POST/posts/P/comments' : ["http://api-bloggyblog404.herokuapp.com/posts/", "/comments/?format=json"]
+			'POST/posts/P/comments' : ["http://api-bloggyblog404.herokuapp.com/posts/", "/comments/"] #Not implemented
 			}
 
 
