127935fdf5ac70e432859a953217e89d5e6f0922
Weling5 <weiling588885@gmail.com>
2016-11-28 13:43:45 -0700

    test

--- Server/post_comment_helpers.py
+++ Server/post_comment_helpers.py
@@ -558,19 +558,16 @@ class AuthorToAuthorPost(Resource):
                             print "I AM HERE 3"
                             nodes = handler.getConnectedNodes()
                             for node in nodes:
-#                                headers = createAuthHeaders(node)
-#
-#                                node_user = db.session.query(Servers).filter(Servers.IP == node).first()
-#                                node_user_name = node_user.user_name
-#                                node_user_pass = node_user.password
-#                                headers['Content-type'] = 'application/json'
-#                                headers['author_id'] = APP_state["session_ids"][sessionID]
-
                                 headers = createAuthHeaders(node)
+
+
                                 headers['Content-type'] = 'application/json'
+                                headers['author_id'] = APP_state["session_ids"][sessionID]
+                                
                                 node_user = db.session.query(Servers).filter(Servers.IP == node).first()
                                 node_user_name = node_user.user_name
                                 node_user_pass = node_user.password
+
                       
                                 [prefix, suffix] = getAPI(node, 'GET/author/A/posts')
                                 custom_url = prefix + author_id +suffix
