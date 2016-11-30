3def38dbed8cc85a4bcfbf7d7bf5068f05dd7064
r-hu1 <randyhu3089@gmail.com>
2016-11-30 10:09:59 -0700

    fix server errors

--- Server/post_comment_handlers.py
+++ Server/post_comment_handlers.py
@@ -185,14 +185,16 @@ class RestHandlers():
                 rst.append([post_self, self.getAuthor(post_self.author_id), self.getComments(post_self.post_id)])
         return rst
 
-    def get_all_post_id (self, param=None):
+    def get_all_post_id (self, post_id):
     
-        all_post = db.session.query(Posts).all()
-        rst = []
-        if len(all_post)>0:
-            for all_comment in all_post:
-                rst += all_comment.post_id
-        return rst
+        all_post = db.session.query(Posts).filter(Posts.post_id == post_id).all()
+#        rst = []
+        if len(all_post)==1:
+#            for all_comment in all_post:
+#                rst += all_comment.post_id
+            return all_post.post_id
+        else:
+            return []
     
     
 
