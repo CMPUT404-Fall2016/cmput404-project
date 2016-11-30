5d73dfb740444dc70ce0afbfc7aa1affa8b98336
r-hu1 <randyhu3089@gmail.com>
2016-11-30 01:47:26 -0700

    fix server errors

--- Server/post_comment_helpers.py
+++ Server/post_comment_helpers.py
@@ -352,12 +352,7 @@ class Post(Resource):
                                 print "_______ this is singlr post____________"
                                 print recvJson
                                 print "______________end___________________"
-                                
-                                for single_item in recvJson["posts"]:
-                                    if single_item["id"] == post_id:
-                                
-                                        json_return["posts"].extend(single_item)
-                                        break
+                                json_return["posts"].extend(recvJson["posts"])
 
                                 
 #                            json_return["posts"].extend(foreign_return["posts"])
